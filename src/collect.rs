cb6128277ec7f27d27436df32ede4d3018f6ad98
Jed Davis <jld@mozilla.com>
2016-10-11 12:48:41 -0600

    Add MPL 2.0 license text and declarations.

--- src/collect.rs
+++ src/collect.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 // FIXME: this module is no longer about embedding in code; should be renamed.
 
 use std::cmp::{Ord, Ordering};
